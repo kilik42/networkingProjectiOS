643b3e8f26bed4c9b1b1840c853cbf2670b0bbc2
marvin evins <maevins@cps.edu>
2020-07-10 06:16:23 -0500

    working on async and dispatching to main queue

--- todoProjectNetworking/Services/NetworkService.swift
+++ todoProjectNetworking/Services/NetworkService.swift
@@ -21,32 +21,36 @@ class NetworkService{
         let url = URL(string: "\(URL_BASE)")!
         
         let task = session.dataTask(with: url) { (data, response, error) in
-            //print(data)
-            if let error = error{
-                debugPrint(error.localizedDescription)
-                return
-            }
-            guard let data = data, let response = response as? HTTPURLResponse else{
-                debugPrint("invalid data or response")
-                return
-            }
-            do{
-                if response.statusCode == 200 {
-                    //parse successful result (todos)
-                    let items = try JSONDecoder().decode(Todos.self, from: data)
-                    onSuccess(items)
-                    print(items)
-                    //handle error
-                    
-                } else{
-                    //show error to user
-                    let err = try JSONDecoder().decode(APIError.self, from: data)
-                    
-                    //handle success
+            
+            DispatchQueue.main.async{
+                if let error = error{
+                    debugPrint(error.localizedDescription)
+                    return
                 }
-            }catch{
-                debugPrint(error.localizedDescription)
+                guard let data = data, let response = response as? HTTPURLResponse else{
+                    debugPrint("invalid data or response")
+                    return
+                }
+                do{
+                    if response.statusCode == 200 {
+                        //parse successful result (todos)
+                        let items = try JSONDecoder().decode(Todos.self, from: data)
+                        onSuccess(items)
+                        print(items)
+                        //handle error
+                        
+                    } else{
+                        //show error to user
+                        let err = try JSONDecoder().decode(APIError.self, from: data)
+                        
+                        //handle success
+                    }
+                }catch{
+                    debugPrint(error.localizedDescription)
+                }
             }
+            
+            
             
             
         }
